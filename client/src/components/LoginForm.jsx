da051ccb010a912afbb47c14d15abeef5516e8ad
Ashley Wright <ashleywright@iMac.local>
2024-10-08 14:20:40 -0400

    did npm install and added node to gitignore

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../graphql/mutations';
+import { LOGIN_USER } from '/Users/ashleywright/bootcamp/Book-Search-Engine/server/graphql/mutations.js'
 
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
